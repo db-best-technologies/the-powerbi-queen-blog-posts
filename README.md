c2e1fe71ab3e05f9a77cc9bd94d7ebf39edc4c05
Bill Ramos <billramo@hotmail.com>
2020-02-18 19:58:39 -0800

    Initial files from Bill added to the master branch

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# the-powerbi-queen-blog-posts
-Content for DeNisha Malone's blog posts 
+![](./media/dbbest-logo-small.png)
+![](./media/DeNisha - Power BI Queen.jpg)
+# The-PowerBI-Queen0Blog-Posts
+Source files for my blogs on Power BI. You can follow my blog series at (https://www.dbbest.com/blog/category/bloggers/denisha-malone/).
+You can also follow DeNisha on Twitter as (https://twitter.com/thepowerbiqueen/)
---
+++ Updating SSO.GOV Power BI First Names Report with PowerShell/ExtractFilesFrom Zip.ps1
@@ -0,0 +1,11 @@
+# Download SSO.Gov ZIP file with first names by state
+Invoke-WebRequest "https://www.ssa.gov/oact/babynames/state/namesbystate.zip" `
+               -OutFile "C:\Temp\NamesByState.zip"
+
+# Extract files from ZIP file onto local computer
+Expand-Archive -LiteralPath "C:\Temp\NamesByState.zip" `
+               -DestinationPath "C:\Temp\NamesByZip" 
+
+# TODO: Once the TXT files are extracted, 
+#       - Append all the TXT files into a single CSV file for PBI.
+#       - Use nice column names for the columns. 
